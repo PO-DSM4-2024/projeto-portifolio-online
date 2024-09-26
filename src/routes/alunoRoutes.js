968468b6b815a60d9674bf8b0532b50a04f840c2
Victor Luís <victorluiscaguiar@gmail.com>
2024-09-26 17:56:27 -0300

    feat: add middleware de autenticação

--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { getAlunos, createAluno, getAlunoById, updateAluno, deleteAluno } from '../controllers/alunoController.js';
+import { verifyToken } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.get('/alunos', getAlunos);
+router.get('/alunos', verifyToken, getAlunos);
 router.post('/alunos', createAluno);
 router.get('/alunos/:id', getAlunoById);
 router.put('/alunos/:id', updateAluno);
